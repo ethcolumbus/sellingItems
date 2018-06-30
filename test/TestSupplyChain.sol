3f11d52d22521aae12cdd5ab07278b31f7bdfca8
Daniel Shapiro <dan@Daniels-MacBook-Pro.local>
2018-06-30 17:08:22 -0400

    trying to write solidity tests

--- test/TestSupplyChain.sol
+++ test/TestSupplyChain.sol
@@ -12,7 +12,23 @@ contract TestSupplyChain {
     // buyItem
 
     // test for failure if user does not send enough funds
+    function testThatUserNeedsEnoughFunds() public {
+      SupplyChain supplyChain = SupplyChain(DeployedAddresses.SupplyChain());
+      supplyChain.addItem("something", 3);
+      bool result = address(supplyChain).call.value(5)(bytes4(keccak256("buyItem(uint)", 0)));
+      Assert.isFalse(result, "able to buy item with less funds than required");
+      //uint expected = 1;
+      //supplyChain.buyItem(0);
+      //uint actual = supplyChain.fetchItem(0)[3];
+      //Assert.isEqual(actual, expected, "able to buy item with less funds than required");
+    }
     // test for purchasing an item that is not for Sale
+    function testThatYouCantSellItemsThatArentForSale() public {
+      //supplyChain.addItem("something else", 3);
+      //address(supplyChain).call.value(5)(bytes4(keccak256("buyItem(uint)", 1)));
+      //bool result = address(supplyChain).call.value(5)(bytes4(keccak256("buyItem(uint)", 1)));
+      //Assert.isFalse(result, "able to buy item that's not for sale");
+    }
 
 
     // shipItem
